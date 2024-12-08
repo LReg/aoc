96d02715445c642b1d8403c04d819d481ac84c6b
abbing <lreg0@web.de>
2024-12-08 17:25:04 +0100

    21 day 11

--- 2021/day11/main.go
+++ 2021/day11/main.go
@@ -16,10 +16,7 @@ func part1() {
 	sum := 0
 	lines := h.GetLinesAsSlice()
 	grid := h.ConvertLinesToGrid(lines)
-	for step := 1; step <= 3; step++ {
-		h.PrintGrid(grid)
-		fmt.Println("--- STEP ---")
-
+	for step := 1; step <= 100; step++ {
 		flashed := make([]h.Point, 0)
 		// inc + 1 of F
 		grid.ForEachPoint(func(p h.Point) {
@@ -28,18 +25,14 @@ func part1() {
 				grid.Set(p, strconv.Itoa(prevNum + 1)[0])
 			} else {
 				grid.Set(p, 'F')
+				sum++
 				flashed = append(flashed, p)
 			}
 		})
 
-		h.PrintGrid(grid)
-		println()
-
 		for {
-			flashedAgain := false
-			fmt.Println("flashed ones: ", flashed)
+			flashedAgain := make([]h.Point, 0)
 			for _, fl := range flashed {
-				fmt.Println("handling flashed one: ", fl)
 				for _, n := range fl.Neighbours() {
 					if n.IsInGrid(grid) {
 						prev := grid.At(n)
@@ -47,21 +40,20 @@ func part1() {
 						if prev == 'F' {
 							continue
 						} else if prevNum == 9 {
-							flashedAgain = true
-							flashed = append(flashed, n)
+							flashedAgain = append(flashedAgain, n)
+							sum++
 							grid.Set(n, 'F')
 						} else {
 							grid.Set(n, strconv.Itoa(prevNum + 1)[0])
 						}
 					}
 				}
-				fmt.Println("flashed after delete", flashed)
 			}
-			h.PrintGrid(grid)
-			println()
-			if !flashedAgain {
+
+			if len(flashedAgain) == 0 {
 				break
 			}
+			flashed = flashedAgain
 		}
 
 		grid.ForEachPoint(func(p h.Point) {
@@ -74,5 +66,57 @@ func part1() {
 }
 
 func part2() {
+	lines := h.GetLinesAsSlice()
+	grid := h.ConvertLinesToGrid(lines)
+	for step := 1; true; step++ {
+		flashed := make([]h.Point, 0)
+		// inc + 1 of F
+		grid.ForEachPoint(func(p h.Point) {
+			prevNum := grid.AtNum(p)
+			if prevNum < 9 {
+				grid.Set(p, strconv.Itoa(prevNum + 1)[0])
+			} else {
+				grid.Set(p, 'F')
+				flashed = append(flashed, p)
+			}
+		})
 
+		for {
+			flashedAgain := make([]h.Point, 0)
+			for _, fl := range flashed {
+				for _, n := range fl.Neighbours() {
+					if n.IsInGrid(grid) {
+						prev := grid.At(n)
+						prevNum := grid.AtNum(n)
+						if prev == 'F' {
+							continue
+						} else if prevNum == 9 {
+							flashedAgain = append(flashedAgain, n)
+							grid.Set(n, 'F')
+						} else {
+							grid.Set(n, strconv.Itoa(prevNum + 1)[0])
+						}
+					}
+				}
+			}
+
+			if len(flashedAgain) == 0 {
+				break
+			}
+			flashed = flashedAgain
+		}
+
+		anyPNotFlash := false
+		grid.ForEachPoint(func(p h.Point) {
+			if grid.At(p) == 'F' {
+				grid.Set(p, '0')
+			} else {
+				anyPNotFlash = true
+			}
+		})
+		if !anyPNotFlash {
+			fmt.Println(step)
+			break
+		}
+	}
 }
